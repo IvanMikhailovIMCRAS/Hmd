5f6beab0d72daa6143572c3ba0cc11fe957ef43e
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-10-25 17:40:47 +0300

    dpd simulation diblock

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import hoomd
 from hoomd import md
+import numpy as np
 
 
 nl = hoomd.md.nlist.Cell(buffer = 0.4)
 dpd = md.pair.DPD(default_r_cut=1.0, kT=1.0, nlist=nl)
-dpd.params[('A', 'A')] = dict(A=25.0, gamma = 4.5)
-dpd.params[('A', 'B')] = dict(A=25.0, gamma = 4.5)
+dpd.params[('A', 'A')] = dict(A=10.0, gamma = 4.5)
+dpd.params[('A', 'B')] = dict(A=33.7, gamma = 4.5)
 dpd.params[('B', 'B')] = dict(A=25.0, gamma = 4.5)
-dpd.params[('A', 'W')] = dict(A=25.0, gamma = 4.5)
-dpd.params[('B', 'W')] = dict(A=25.0, gamma = 4.5)
+dpd.params[('A', 'W')] = dict(A=75.0, gamma = 20.0)
+dpd.params[('B', 'W')] = dict(A=26.3, gamma = 4.5)
 dpd.params[('W', 'W')] = dict(A=25.0, gamma = 4.5)
 
 harmonic = hoomd.md.bond.Harmonic()
@@ -16,30 +17,30 @@ harmonic.params['AA'] = dict(k=128.0, r0=0.5)
 harmonic.params['BB'] = dict(k=4222.6, r0=0.4125)
 harmonic.params['AB'] = dict(k=4222.6, r0=0.4125)
 
-# angle = hoomd.md.angle.Harmonic()
-# angle.params['CCC'] = dict(k=2*58.35, t0=112.7 * pi / 180)
-# angle.params['CCH'] = dict(k=2*37.5, t0=110.7 * pi / 180)
-# angle.params['HCH'] = dict(k=2*33.0, t0=107.8 * pi / 180)
+cosinesq = hoomd.md.angle.CosineSquared()
+cosinesq.params['AAA'] = dict(k=2*30.0, t0=np.pi)
+cosinesq.params['AAB'] = dict(k=2*30.0, t0=np.pi)
+cosinesq.params['ABB'] = dict(k=2*16.4946, t0=130.0*np.pi/180)
+cosinesq.params['BBB'] = dict(k=2*16.4946, t0=130.0*np.pi/180)
 
-# harmonic = hoomd.md.bond.harmonic()
-# harmonic.bond_coeff.set('polymer', k=330.0, r0=0.84)
 gpu = hoomd.device.GPU()
 sim = hoomd.Simulation(device=gpu, seed=200)
 sim.create_state_from_gsd(filename='init_config.gsd')
 integrator = hoomd.md.Integrator(dt=0.01)
 
 nve = hoomd.md.methods.ConstantVolume(filter=hoomd.filter.All())
-sim.operations.integrator.methods = [nve]
 sim.operations.integrator = integrator
+sim.operations.integrator.methods = [nve]
 integrator.forces.append(dpd)
+integrator.forces.append(harmonic)
+integrator.forces.append(cosinesq)
 
 # Set up output
-gsd = hoomd.write.GSD(trigger=100, filename='output.gsd')
+gsd = hoomd.write.GSD(trigger=20, filename='output.gsd')
 sim.operations.writers.append(gsd)
 log = hoomd.logging.Logger()
 log += dpd
 gsd.log = log
 
-
-sim.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=1.0)
-sim.run(1e3)
+sim.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=0.0)
+sim.run(5e4)
